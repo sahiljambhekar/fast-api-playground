368abaf0f64b717eff2fe83e574bf0e59ead9641
Sahil <6189197+sahiljambhekar@users.noreply.github.com>
2024-11-16 22:56:06 -0500

    Update Docker image tag to version 0.1.0 and extract version from pyproject.toml in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export VIRTUAL_ENV=".venv" # uv
+# Extract version from pyproject.toml
+VERSION=$(yq .project.version pyproject.toml)
+
 docker build . \
--t ghcr.io/sahiljambhekar/fast-api-playground:worldpop-latest \
+-t ghcr.io/sahiljambhekar/fast-api-playground:worldpop-${VERSION} \
 -f ops/Dockerfile
